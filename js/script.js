c5f99d51545a9788187b2e90271817d43b425bab
César Aparicio <clark012389@hotmail.com>
2021-01-25 02:53:56 -0600

    Menu implementation for desktop devices

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-console.log('Script cargado')
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector("#header");
+  const linksMenu = document.querySelectorAll(".menu li a");
+
+  console.log("LINKS", linksMenu);
+
+  /**
+   * For Scroll Menu Animation
+   * */
+  window.addEventListener("scroll", function () {
+    let windowHeight = document.documentElement.scrollTop;
+
+    if (windowHeight > 100) {
+      header.classList.add("header--fixed");
+    }
+    if (windowHeight <= 100) {
+      header.classList.remove("header--fixed");
+    }
+  });
+
+  /**
+   * For animation of menu items active
+   */
+
+  for (const [index, link] of linksMenu.entries()) {
+    // console.log(value);
+    link.addEventListener("click", () => {
+      console.log("CLICK a: ", link);
+      let linkActive = document.querySelector(".menu__item--active");
+      linkActive.classList.remove("menu__item--active");
+      linksMenu[index].classList.add("menu__item--active");
+    });
+  }
+});
