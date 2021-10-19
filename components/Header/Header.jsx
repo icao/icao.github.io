51d4c15055b97806ce0756240aae1b3ab33dfbb3
César Aparicio <clark012389@hotmail.com>
2021-10-19 02:18:22 -0500

    Implemented  clsx dependency

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,39 +1,44 @@
-// import styles from '../styles/Home.module.scss'
+import { useState } from 'react'
+import clsx from 'clsx'
 import styles from './Header.module.scss'
 
 const Header = () => {
-  return (<header id="header" className={styles.header}>
-    <nav className={styles.nav}>
-      <div className={styles['nav-mobile']}>
-        <div className={styles.logo}>
-          <span className={styles.logo__typo}>César <strong>Aparicio </strong></span>
-        </div>
-        <div className={styles['menu-hamburger']}>
-          <div id={styles['icon-hamburger']}>
-            <span></span>
-            <span></span>
-            <span></span>
+
+  let [isOpen, setIsOpen] = useState(true)
+
+  return (
+    <header id="header" className={clsx(styles.header, { [styles['header--open']]: isOpen })}>
+      <nav className={styles.nav}>
+        <div className={styles['nav-mobile']}>
+          <div className={styles.logo}>
+            <span className={styles.logo__typo}>César <strong>Aparicio </strong></span>
+          </div>
+          <div className={styles['menu-hamburger']}>
+            <div id={styles['icon-hamburger']} className={clsx({ [styles['icon-hamburger--open']]: isOpen })}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <ul id="menu" className={styles.menu}>
-        <li className={styles.menu__item}>
-          <a href="#home" className={`${styles['menu__item-link']} ${styles['menu__item--active']}`}
-          >Inicio</a>
-        </li>
-        <li className={styles.menu__item}>
-          <a href="#resume" className={styles['menu__item-link']}>Experiencia</a>
-        </li>
-        <li className={styles.menu__item}>
-          <a href="#skills" className={styles['menu__item-link']}>Skills</a>
-        </li>
-        <li className={styles.menu__item}>
-          <a href="#works" className={styles['menu__item-link']}>Proyectos</a>
-        </li>
-      </ul>
-    </nav>
-  </header>);
+        <ul id="menu" className={clsx(styles.menu, { [styles['menu--open']]: isOpen })}>
+          <li className={styles.menu__item}>
+            <a href="#home" className={clsx(styles['menu__item-link'], styles['menu__item--active'])}
+            >Inicio</a>
+          </li>
+          <li className={styles.menu__item}>
+            <a href="#resume" className={styles['menu__item-link']}>Experiencia</a>
+          </li>
+          <li className={styles.menu__item}>
+            <a href="#skills" className={styles['menu__item-link']}>Skills</a>
+          </li>
+          <li className={styles.menu__item}>
+            <a href="#works" className={styles['menu__item-link']}>Proyectos</a>
+          </li>
+        </ul>
+      </nav>
+    </header >);
 }
 
 export default Header;
