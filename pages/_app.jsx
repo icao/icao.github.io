c3ca58de2b64d18bd51e06bbf6ffe18ab0d218fd
cesar-aparicio <cesar@thirty3.ch>
2021-10-31 01:42:52 -0500

    Test lint staged 2

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,9 +2,8 @@ import '../styles/globals.scss'
 
 /* eslint-disable react/prop-types */
 function MyApp({ Component, pageProps }) {
-  console.log('haskdhjkas', typeof Component, typeof pageProps)
+  /* eslint-enable react/prop-types */
   return <Component {...pageProps} />
 }
-/* eslint-enable react/prop-types */
 
 export default MyApp
