b0e613bc0f2a0ea18dc0fac8fcd1379cb9b26f8d
César Aparicio <clark012389@hotmail.com>
2021-12-13 18:47:06 -0600

    Fix erro  crossOrigin in the console

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,11 @@ export default function Index() {
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
