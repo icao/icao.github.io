6a092832df6bde7d347c209b96aff2ea93e0e7b6
César Aparicio <clark012389@hotmail.com>
2022-02-10 21:36:18 -0600

    Added TimelineMoment Component

---
+++ components/Common/TimelineMoment/TimelineMoment.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+import styles from './TimelineMoment.module.scss'
+
+const TimelineMoment = ({
+  children,
+  arrowUp,
+  arrowRight,
+  arrowLeft,
+  arrowDown,
+}) => (
+  <div
+    className={clsx(
+      { [styles['moment__up-arrow']]: arrowUp },
+      { [styles['moment__left-arrow']]: arrowLeft },
+      { [styles['moment__down-arrow']]: arrowDown },
+      { [styles['moment__right-arrow']]: arrowRight }
+    )}
+  >
+    {children}
+  </div>
+)
+
+TimelineMoment.propTypes = {
+  children: PropTypes.node.isRequired,
+  arrowUp: PropTypes.bool,
+  arrowRight: PropTypes.bool,
+  arrowDown: PropTypes.bool,
+  arrowLeft: PropTypes.bool,
+}
+
+TimelineMoment.defaultProps = {
+  arrowUp: false,
+  arrowRight: false,
+  arrowDown: false,
+  arrowLeft: false,
+}
+
+export default TimelineMoment
