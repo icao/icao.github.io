8231a94f0316581dbee2c428e375106c3f8a2faa
César Aparicio <clark012389@hotmail.com>
2022-03-07 02:31:09 -0600

    Added dynamic load of projects

---
+++ assets/projects.js
@@ -0,0 +1,93 @@
+export default [
+  {
+    id: 'ICAO01',
+    urlImage: '/images/amk.jpeg',
+    title: 'AMK Technologies',
+    type: 'Sitio Web',
+    descriptions: {
+      paragraphs: [
+        'Fue un sitio web empresarial con diseño personalizado provisto por el área de diseño.',
+        'Se enfocó en el manejo de sliders y tabs con sliders, así como pixel perfect.',
+        'El sitio fue hecho responsivo cuidando la semántica HTML para garantizar el SEO, se procuró optimizar lo más posible en la carga de las imágenes, aunque fue limitado por el tiempo de entrega.',
+      ],
+      technologies: ['SCSS', 'HTML5', 'Javascript(ES6)', 'Gulp Task Runner'],
+    },
+    link: 'https://bit.ly/3MvMX67',
+  },
+  {
+    id: 'ICAO02',
+    urlImage: '/images/photo02.jpeg',
+    title: 'The World Times',
+    type: 'Aplicación Web',
+    descriptions: {
+      paragraphs: [
+        'Aplicación web de noticias del día hecho con React.',
+        'Se desarrolló bajo un diseño propio y la garantía de ser responsivo para todos los dispositivos móviles, se implementó un diseño  masonry usando CSS Grid Layout',
+        'Se usaron los ciclos de vida de React para suscribir events listeners y generar el Masonry Grid, así como también para el llamado de un API RESTful que provee la lista de artículos. Se consumió el servicio gratuito API News de Mediastack.',
+      ],
+      technologies: [
+        'Reactjs',
+        'Materialize',
+        'Custom CSS',
+        'API News de Mediastack',
+      ],
+    },
+    link: 'http://project-04-the-world-times.surge.sh',
+  },
+  {
+    id: 'ICAO03',
+    urlImage: '/images/photo03.jpeg',
+    title: 'Browser Movies',
+    type: 'Aplicación Web',
+    descriptions: {
+      paragraphs: ['Aplicación web, buscador de reseña de peliculas.'],
+      technologies: ['dummy'],
+    },
+    link: 'http://project-02-search-movies-v2.surge.sh/',
+  },
+  {
+    id: 'ICAO04',
+    urlImage: '/images/photo04.jpeg',
+    title: 'Evently',
+    type: 'Aplicación Web',
+    descriptions: {
+      paragraphs: ['Aplicación web para buscar imagenes de stock.'],
+      technologies: ['dummy'],
+    },
+    link: 'http://project-05-context-api-buscador-imagenes.surge.sh/',
+  },
+  {
+    id: 'ICAO05',
+    urlImage: '/images/photo05.jpg',
+    title: 'The World Times',
+    type: 'Open Source',
+    descriptions: {
+      paragraphs: ['Dummy'],
+      technologies: ['dummy'],
+    },
+    link: 'dummy',
+  },
+  {
+    id: 'ICAO06',
+    urlImage:
+      'https://images.pexels.com/photos/9749273/pexels-photo-9749273.jpeg',
+    title: 'Map Finder Routes',
+    type: 'Progressive Web App',
+    descriptions: {
+      paragraphs: ['Dummy'],
+      technologies: ['dummy'],
+    },
+    link: 'dummy',
+  },
+  {
+    id: 'ICAO07',
+    urlImage: '/images/photo07.jpg',
+    title: 'Doesnt Matter, dont worry about it',
+    type: 'Open Source',
+    descriptions: {
+      paragraphs: ['Dummy'],
+      technologies: ['dummy'],
+    },
+    link: 'dummy',
+  },
+]
--- components/Layout/Projects/Projects.jsx
+++ components/Layout/Projects/Projects.jsx
@@ -3,153 +3,82 @@ import Image from 'next/image'
 import Card from '@components/Common/Card/Card'
 import Modal from '@components/Common/Modal/Modal'
 import Link from 'next/link'
+import projects from '@assets/projects'
 import styles from './Projects.module.scss'
 
 function Projects() {
   const [isOpenModal, setIsOpenModal] = useState(false)
   const [modalContent, setModalContent] = useState(null)
-  const projects = [
-    {
-      id: 'ICAO01',
-      urlImage: '/images/photo01.jpeg',
-      title: 'The player winner',
-      ramonas: '/public/images/photo04.jpg',
-      type: 'Open Source',
-    },
-    {
-      id: 'ICAO02',
-      urlImage: '/images/photo02.jpeg',
-      title: 'VSCODE Extension',
-      ramonas: '/public/images/photo05.jpg',
-      type: 'Open Source',
-    },
-    {
-      id: 'ICAO03',
-      urlImage: '/images/photo03.jpeg',
-      title: 'CODEPEN',
-      ramonas: '/public/images/photo06.jpg',
-      type: 'Landing Page',
-    },
-    {
-      id: 'ICAO04',
-      urlImage: '/images/photo04.jpeg',
-      title: 'VSCODE Theme',
-      ramonas: '/public/images/photo06.jpg',
-      type: 'Photography',
-    },
-    {
-      id: 'ICAO05',
-      urlImage: '/images/photo05.jpg',
-      title: 'The World Times',
-      ramonas: '/public/images/photo06.jpg',
-      type: 'Open Source',
-    },
-    {
-      id: 'ICAO06',
-      urlImage:
-        'https://images.pexels.com/photos/9749273/pexels-photo-9749273.jpeg',
-      title: 'Map Finder Routes',
-      ramonas: '/public/images/photo06.jpg',
-      type: 'Progressive Web App',
-    },
-    {
-      id: 'ICAO07',
-      urlImage: '/images/photo07.jpg',
-      title: 'Doesnt Matter, dont worry about it',
-      ramonas: '/public/images/photo06.jpg',
-      type: 'Open Source',
-    },
-  ]
+
+  const generateModal = (urlImage, title, paragraphs, technologies, link) => {
+    setModalContent(
+      <div className={styles.detail__container}>
+        <div className={('next__image-container', styles.detail__image)}>
+          <Image src={urlImage} layout="fill" className="next__image--cover" />
+        </div>
+        <div className={styles.detail__content}>
+          <h2 className={styles.detail__title}>{title}</h2>
+          {paragraphs.map((paragraph, index) => (
+            <p key={`${index + 1}`} className={styles.detail__text}>
+              {paragraph}
+            </p>
+          ))}
+
+          <p className={styles.detail__text}>Tecnologias usadas:</p>
+          <ul className="ul__emphasis">
+            {technologies.map((technology, index) => (
+              <li key={`${index + 1}`} className={styles.detail__list}>
+                {technology}
+              </li>
+            ))}
+          </ul>
+          <div className={styles.detail__footer}>
+            <Link href={link} passHref>
+              <a
+                href="replace"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button--call-to-action"
+              >
+                VER PROYECTO
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
       {isOpenModal && <Modal showModal={setIsOpenModal}>{modalContent}</Modal>}
-
       <section className="collection">
-        {projects.map((project) => (
-          <div
-            key={project.id}
-            onClick={() => {
-              setIsOpenModal(true)
-              setModalContent(
-                <div className={styles.detail__container}>
-                  <div
-                    className={('next__image-container', styles.detail__image)}
-                  >
-                    <Image
-                      // src="/images/photo02.jpeg"
-                      src="https://images.pexels.com/photos/9749273/pexels-photo-9749273.jpeg"
-                      layout="fill"
-                      className="next__image--cover"
-                    />
-                  </div>
-                  <div className={styles.detail__content}>
-                    <h2 className={styles.detail__title}>{project.title}</h2>
-                    <p className={styles.detail__text}>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Assumenda officiis iure deserunt expedita pariatur ullam
-                      sed id, voluptatum, quis eligendi obcaecati, at porro
-                      voluptate aspernatur.
-                    </p>
-                    <p className={styles.detail__text}>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      Illum, fugiat quam! Eos voluptates enim vero?
-                    </p>
-                    <p className={styles.detail__text}>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Recusandae odit fuga iusto.
-                    </p>
-
-                    <p className={styles.detail__text}>
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                      Et dignissimos sapiente laudantium, fugiat rem debitis sit
-                      corporis voluptates, odit nihil sed itaque laboriosam,
-                      reiciendis cum?
-                    </p>
-
-                    <p className={styles.detail__text}>Lorem ipsum:</p>
-
-                    <ul className="ul__emphasis">
-                      <li className={styles.detail__list}>Lorem, ipsum.</li>
-                      <li className={styles.detail__list}>
-                        Opera rotas, tenet ipsum dolor sit romen fergen tom
-                        amet.
-                      </li>
-                      <li className={styles.detail__list}>Lorem dolor.</li>
-                      <li className={styles.detail__list}>Iplor sit amet.</li>
-                      <li className={styles.detail__list}>
-                        Adipisci consequuntur nostrum{' '}
-                      </li>
-                    </ul>
-                    <div className={styles.detail__footer}>
-                      <Link
-                        href="https://www.linkedin.com/in/cesaricao/"
-                        passHref
-                      >
-                        <a
-                          href="replace"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="button--call-to-action"
-                        >
-                          VER PROYECTO
-                        </a>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              )
-            }}
-            aria-hidden="true"
-          >
-            <Card
-              urlImage={project.urlImage}
-              title={project.title}
-              tag={project.type}
-              designAlternative
-            />
-          </div>
-        ))}
+        {projects.map(
+          ({
+            id,
+            urlImage,
+            title,
+            type,
+            descriptions: { paragraphs, technologies },
+            link,
+          }) => (
+            <div
+              key={id}
+              onClick={() => {
+                setIsOpenModal(true)
+                generateModal(urlImage, title, paragraphs, technologies, link)
+              }}
+              aria-hidden="true"
+            >
+              <Card
+                urlImage={urlImage}
+                title={title}
+                tag={type}
+                designAlternative
+              />
+            </div>
+          )
+        )}
       </section>
     </>
   )
